f5bc7d64d6e2d4f105f50dfece9cb766776254c2
柏霜 <wenle.bwl@alibaba-inc.com>
2024-04-08 20:47:35 +0800

    add cookie

--- src/main/java/wenle/github/com/helloworldweb/HelloController.java
+++ src/main/java/wenle/github/com/helloworldweb/HelloController.java
@@ -2,6 +2,7 @@ package wenle.github.com.helloworldweb;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collection;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    public String printResponseHeaders(HttpServletResponse response) {
+        // 获取所有的Header名称
+        Collection<String> headerNames = response.getHeaderNames();
+
+        // 打印每个Header的名称和对应的值
+        StringBuilder sb = new StringBuilder();
+        for (String headerName : headerNames) {
+            String headerValue = response.getHeader(headerName);
+            sb.append(headerName + ": " + headerValue);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
     @GetMapping("/")
     public void index(HttpServletResponse response) {
         CookieUtil.addCookie("key", "value", response);
-        String header = response.getHeader("Set-Cookie2");
-        String responseContent = String.format("Set-Cookie2 header: %s", header);
         response.setContentType("text/plain");               // 设置内容类型为纯文本
         response.setCharacterEncoding("UTF-8");             // 设置字符编码
 
+        String responseContent = String.format("headers:\n%s", printResponseHeaders(response));
         try (PrintWriter writer = response.getWriter()) {
             writer.write(responseContent);                  // 写入响应内容
             writer.flush();                                 // 清空缓冲，完成响应内容的发送
